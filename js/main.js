b9ec1ddeb1084a02be242e0e5dcf237ebc355f60
fumiaki49 <me.fk39@gmail.com>
2020-06-24 11:43:37 +0900

    my works にmodal機能を追加

--- js/main.js
+++ js/main.js
@@ -4,4 +4,13 @@ $(function () {
     var sameIndex = $('.scroll-target').eq(index).offset().top - 83;
     $('html, body').animate({scrollTop: sameIndex});
   });
+
+  $('.my-works__list__item').click(function() {
+    var modalData = $(this).data('modal-link');
+    var sameItem = document.querySelector('.' + modalData);
+    $(sameItem).fadeIn();
+  })
+  $('.modal__content-box__close-btn').click(function() {
+    $('.modal').fadeOut();
+  });
 });
